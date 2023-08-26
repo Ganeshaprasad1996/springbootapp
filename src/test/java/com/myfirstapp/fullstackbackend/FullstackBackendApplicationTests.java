101203809ef629e52a5b563da537398571ca6490
Ganeshaprasad1996 <prasadgk107@gmail.com>
2023-08-26 20:12:41 +0530

    framework added

---
+++ src/test/java/com/myfirstapp/fullstackbackend/FullstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfirstapp.fullstackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
