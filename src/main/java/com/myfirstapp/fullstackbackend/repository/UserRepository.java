6a915fbe3acbefa31c5591604b494aba7c4fc3ad
Ganeshaprasad <88617330+Ganeshaprasad1996@users.noreply.github.com>
2023-08-26 21:07:08 +0530

    connected database

---
+++ src/main/java/com/myfirstapp/fullstackbackend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.myfirstapp.fullstackbackend.repository;
+
+import com.myfirstapp.fullstackbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
